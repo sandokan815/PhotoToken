8dcbba938d540e8ea4747d1612f5646724c5d3f9
JavascriptNinja815 <xandokan815@protonmail.com>
2018-05-09 17:21:46 +0200

    login/market page modified

--- src/routes/marketplace/MarketPlace.js
+++ src/routes/marketplace/MarketPlace.js
@@ -1,61 +1,67 @@
 import React from 'react';
+import { Row, Col } from 'react-bootstrap';
+import bootstrap from 'bootstrap/dist/css/bootstrap.min.css';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './MarketPlace.css';
-import bootstrap from 'bootstrap/dist/css/bootstrap.min.css';
-import {Row, Col, Button} from 'react-bootstrap';
 import placeholder from '../assets/placeholder.png';
 
 class MarketPlace extends React.Component {
-
   render() {
     return (
       <div className={s.root}>
         <div className={s.container}>
-          <div className={s.title}>
-            Marketplace
-          </div>
+          <div className={s.title}>Marketplace</div>
           <Row className={s.row}>
             <Col md={4}>
               <div className={s.photoPosition}>
-                <img src={placeholder}/>
+                <img src={placeholder} alt="temp" />
               </div>
               <div className={s.content}>
                 <div>Seller: Hossein123</div>
                 <div>Subject: Alfred Hitchcock</div>
                 <div>Photographer: Dan Wynn</div>
-                <div>Minium bid: 100ETH</div>
+                <div>Minimum bid: 100ETH</div>
                 <div>
-                  <a><button className={s.button}>Place Bid</button></a>
+                  {/* <a>
+                    <button className={s.button}>Place Bid</button>
+                  </a> */}
+                  <button className={s.button}>Place Bid</button>
                 </div>
               </div>
             </Col>
 
             <Col md={4}>
               <div className={s.photoPosition}>
-                <img src={placeholder}/>
+                <img src={placeholder} alt="temp" />
               </div>
               <div className={s.content}>
                 <div>Seller: Hossein123</div>
                 <div>Subject: Alfred Hitchcock</div>
                 <div>Photographer: Dan Wynn</div>
-                <div>Minium bid: 100ETH</div>
+                <div>Minimum bid: 100ETH</div>
                 <div>
-                  <a><button className={s.button}>Place Bid</button></a>
+                  {/* <a>
+                    <button className={s.button}>Place Bid</button>
+                  </a> */}
+                  <button className={s.button}>Place Bid</button>
                 </div>
               </div>
             </Col>
 
             <Col md={4}>
               <div className={s.photoPosition}>
-                <img src={placeholder}/>
+                <img src={placeholder} alt="temp" />
               </div>
               <div className={s.content}>
                 <div>Seller: Hossein123</div>
                 <div>Subject: Alfred Hitchcock</div>
                 <div>Photographer: Dan Wynn</div>
-                <div>Minium bid: 100ETH</div>
+                <div>Minimum bid: 100ETH</div>
                 <div>
-                  <a><button className={s.button}>Place Bid</button></a>
+                  {/* <a>
+                    <button className={s.button}>Place Bid</button>
+                  </a> */}
+                  <button className={s.button}>Place Bid</button>
                 </div>
               </div>
             </Col>
@@ -63,45 +69,54 @@ class MarketPlace extends React.Component {
           <Row className={s.row}>
             <Col md={4}>
               <div className={s.photoPosition}>
-                <img src={placeholder}/>
+                <img src={placeholder} alt="temp" />
               </div>
               <div className={s.content}>
                 <div>Seller: Hossein123</div>
                 <div>Subject: Alfred Hitchcock</div>
                 <div>Photographer: Dan Wynn</div>
-                <div>Minium bid: 100ETH</div>
+                <div>Minimum bid: 100ETH</div>
                 <div>
-                  <a><button className={s.button}>Place Bid</button></a>
+                  {/* <a>
+                    <button className={s.button}>Place Bid</button>
+                  </a> */}
+                  <button className={s.button}>Place Bid</button>
                 </div>
               </div>
             </Col>
 
             <Col md={4}>
               <div className={s.photoPosition}>
-                <img src={placeholder}/>
+                <img src={placeholder} alt="temp" />
               </div>
               <div className={s.content}>
                 <div>Seller: Hossein123</div>
                 <div>Subject: Alfred Hitchcock</div>
                 <div>Photographer: Dan Wynn</div>
-                <div>Minium bid: 100ETH</div>
+                <div>Minimum bid: 100ETH</div>
                 <div>
-                  <a><button className={s.button}>Place Bid</button></a>
+                  {/* <a>
+                    <button className={s.button}>Place Bid</button>
+                  </a> */}
+                  <button className={s.button}>Place Bid</button>
                 </div>
               </div>
             </Col>
 
             <Col md={4}>
               <div className={s.photoPosition}>
-                <img src={placeholder}/>
+                <img src={placeholder} alt="temp" />
               </div>
               <div className={s.content}>
                 <div>Seller: Hossein123</div>
                 <div>Subject: Alfred Hitchcock</div>
                 <div>Photographer: Dan Wynn</div>
-                <div>Minium bid: 100ETH</div>
+                <div>Minimum bid: 100ETH</div>
                 <div>
-                  <a><button className={s.button}>Place Bid</button></a>
+                  {/* <a>
+                    <button className={s.button}>Place Bid</button>
+                  </a> */}
+                  <button className={s.button}>Place Bid</button>
                 </div>
               </div>
             </Col>
@@ -112,4 +127,4 @@ class MarketPlace extends React.Component {
   }
 }
 
-export default withStyles(bootstrap, s) (MarketPlace);
+export default withStyles(bootstrap, s)(MarketPlace);
