38a812900f6d7ea362365aa8bbcfaa4b348f37d5
JavascriptNinja815 <xandokan815@protonmail.com>
2018-05-10 04:27:40 +0200

    temp

--- src/routes/login/Login.js
+++ src/routes/login/Login.js
@@ -1,13 +1,8 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Login.css';
 
 class Login extends React.Component {
-  // static propTypes = {
-  //   title: PropTypes.string.isRequired,
-  // };
-
   render() {
     return (
       <div className={s.root}>
